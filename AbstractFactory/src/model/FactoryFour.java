4d33245cab9209ad10d7683dd88f6d627136c217
Mortuumm <73663755+Mortuumm@users.noreply.github.com>
2021-03-23 20:34:42 +0300

    Add files via upload

---
+++ AbstractFactory/src/model/FactoryFour.java
@@ -0,0 +1,8 @@
+package model;
+
+public class FactoryFour implements AbstractFactory {
+    @Override
+    public ArrowFour CreateArrow(double startX, double startY, double endX, double endY) {
+        return new ArrowFour(startX, startY, endX, endY);
+    }
+}
